f92eb064e751e04870850d454a7c5e2402717c7d
Rafaelsilva <99143713+Rafazg@users.noreply.github.com>
2022-07-28 21:15:42 -0300

    initial commit

---
+++ src/JavaScript/carrousel.js
@@ -0,0 +1,10 @@
+const prevButton = document.getElementById('arrow-prev')
+const nextButton = document.getElementById('arrow-next')
+const slidesContainer = document.getElementById('slides-container')
+const slide = document.getElementById('slide')
+const clicou = document.getElementById('arrow-next')
+
+nextButton.addEventListener("click", (event) =>{
+    const slideWidth = slide.clientWidth
+    slidesContainer.scrollLeft += slideWidth;
+}); 
